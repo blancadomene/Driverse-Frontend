351e783fcdd1d15e70edd174119ddb0bb6273adb
Blanca <blancadomene@gmail.com>
2021-05-24 09:21:34 +0200

    "Chips background modified, availableDaysOfWeek added, User created"

--- app/src/main/java/com/blancadomene/tfg/availableDaysOfWeek.java
+++ app/src/main/java/com/blancadomene/tfg/availableDaysOfWeek.java
@@ -10,7 +10,13 @@ import android.widget.LinearLayout;
 
 public class availableDaysOfWeek extends LinearLayout {
     View view;
-    int num = 0;
+    View monday;
+    View tuesday;
+    View wednesday;
+    View thursday;
+    View friday;
+    View saturday;
+    View sunday;
 
     public availableDaysOfWeek(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -19,9 +25,22 @@ public class availableDaysOfWeek extends LinearLayout {
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         view = inflater.inflate(R.layout.layout_available_days_of_week, this, true);
+
+        monday = view.findViewById(R.id.monday_chip);
+        tuesday = view.findViewById(R.id.tuesday_chip);
+        wednesday = view.findViewById(R.id.wednesday_chip);
+        thursday = view.findViewById(R.id.thursday_chip);
+        friday = view.findViewById(R.id.friday_chip);
+        saturday = view.findViewById(R.id.saturday_chip);
+        sunday = view.findViewById(R.id.sunday_chip);
+
+        monday.setOnClickListener(v -> v.setActivated(!v.isActivated()));
+        tuesday.setOnClickListener(v -> v.setActivated(!v.isActivated()));
+        wednesday.setOnClickListener(v -> v.setActivated(!v.isActivated()));
+        thursday.setOnClickListener(v -> v.setActivated(!v.isActivated()));
+        friday.setOnClickListener(v -> v.setActivated(!v.isActivated()));
+        saturday.setOnClickListener(v -> v.setActivated(!v.isActivated()));
+        sunday.setOnClickListener(v -> v.setActivated(!v.isActivated()));
     }
 
-    public void ShowNum() {
-        System.out.println(num++);
-    }
 }
